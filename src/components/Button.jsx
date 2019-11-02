ba279ed73768db84282760a5092b815cad315966
Michel Eckhardt <michel.desenvolvedor@gmail.com>
2019-11-02 14:56:13 -0300

    Adicionado logica

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ export default props => {
     classes += props.triple ? 'triple' : ''
 
     return (
-        <button onclick={e => props.click && props.click(props.label)} className={classes}>
+        <button onClick={e => props.click && props.click(props.label)} className={classes}>
             {props.label}
         </button>
     )
