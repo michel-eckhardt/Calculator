e28b2790b68d3009d7e09396de9a650d97dbed92
Michel Eckhardt <michel.desenvolvedor@gmail.com>
2019-11-02 15:07:24 -0300

    Retirado o console.log desnecessario

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -79,7 +79,6 @@ export default class Calculator extends Component {
             const values = [...this.state.values]
             values[i] = newValue
             this.setState({ values })
-            console.log(values)
         }
 
     }
